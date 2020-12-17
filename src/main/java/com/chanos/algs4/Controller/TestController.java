915f29fa4048bf03d31eb47f32b9abc68d767cc9
Kaminari <chankocyo@gmail.com>
2020-12-17 22:01:42 +0800

    Initialize.

---
+++ src/main/java/com/chanos/algs4/Controller/TestController.java
@@ -0,0 +1,7 @@
+package com.chanos.algs4.Controller;
+
+public class TestController {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/chanos/algs4/Algs4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanos.algs4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algs4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
