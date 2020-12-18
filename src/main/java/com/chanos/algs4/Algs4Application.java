82058d0742417bd83b1e494305bc8ee30925ea6e
Kaminari <chankocyo@gmail.com>
2020-12-18 08:04:21 +0800

    Connect to local Mariadb;add dependencies.

--- src/main/java/com/chanos/algs4/Algs4Application.java
+++ src/main/java/com/chanos/algs4/Algs4Application.java
@@ -2,8 +2,13 @@ package com.chanos.algs4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * @author kaminari
+ */
 @SpringBootApplication
+@EnableSwagger2
 public class Algs4Application {
 
     public static void main(String[] args) {
